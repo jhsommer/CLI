ea24d678f2d995ca5d033ce745150f3613c8320a
jhsommer <129003263+jhsommer@users.noreply.github.com>
2025-11-30 23:22:17 +0100

    Update ArgumentParser.cs
    Refactored the whole ArgumentParser to handle more input and handle false input as well

--- TB_CLI/ArgumentParser.cs
+++ TB_CLI/ArgumentParser.cs
@@ -5,19 +5,17 @@ namespace TB_CLI;
 public class ArgumentParser
 {
     private const char ParameterDelimiter = '-';
-    private List<ExpectedArguments> _expectedArgumentsList = [];
+    private readonly List<ExpectedArguments> _expectedArgumentsList = [];
 
     private const string HelpFile = "help.txt";
-    private string pathFile = "path.txt";
-    
-    Scan scanAction = new Scan();
-    Filter filterAction = new Filter();
-    Move moveAction = new Move();
-    Delete deleteAction = new Delete();
-    
-    
-    
+    private const string PathFile = "path.txt";
 
+    private readonly Scan _scanAction = new();
+    private readonly Filter _filterAction = new();
+    private readonly Move _moveAction = new();
+    private readonly Delete _deleteAction = new();
+    private readonly ShowList _showListAction = new();
+    
     public void AddExpectedArguments(ExpectedArguments argument)
     {
         this._expectedArgumentsList.Add(argument);
@@ -27,17 +25,9 @@ public class ArgumentParser
     {
         if (args.Length == 0)
         {
-            
-#if DEBUG
-            if (!File.Exists(HelpFile))
-            {
-                Console.WriteLine("Help file not found.");
-                return;
-            }
-#endif
-            
             string helpText = File.ReadAllText(HelpFile);
             Console.WriteLine(helpText);
+            return;
         }
         
         for (int i = 0; i < args.Length; i++)
@@ -52,14 +42,44 @@ public class ArgumentParser
                     if (potentialAlias == "p")
                     {
                         GetFollowingPath(expectedArgument, args, i);
+                        return;
                     }
 
                     if (potentialAlias == "h")
                     {
-                        string helpText = File.ReadAllText(HelpFile);
-                        Console.WriteLine(helpText);
+                        ShowHelp();
+                        return;
                     }
-                    
+
+                    if (potentialAlias == "s")
+                    {
+                        StartScan();
+                        return;
+                    }
+
+                    if (potentialAlias == "f")
+                    {
+                        StartFilter(args, expectedArgument, i);
+                        return;
+                    }
+
+                    if (potentialAlias == "sl")
+                    {
+                        _showListAction.Action();
+                        return;
+                    }
+
+                    if (potentialAlias == "m")
+                    {
+                        StartMove(args, expectedArgument, i);
+                        return;
+                    }
+
+                    if (potentialAlias == "d")
+                    {
+                        StartDelete();
+                        return;
+                    }
                     
                     
                 }
@@ -72,58 +92,65 @@ public class ArgumentParser
                 {
                     if (potentialFullName == "help")
                     {
-                        string helpText = File.ReadAllText(HelpFile);
-                        Console.WriteLine(helpText);
+                        ShowHelp();
+                        return;
                     }
                     
                     if (potentialFullName == "path")
                     {
                         GetFollowingPath(expectedArgument, args, i);
+                        return;
                     }
 
                     if (potentialFullName == "scan")
                     {
-                        if(!string.IsNullOrWhiteSpace(pathFile))
-                        {
-                            string pathText = File.ReadAllText(pathFile);
-                            scanAction.ScanFiles(pathText);
-                        }
-                        else
-                            Console.WriteLine("No path to scan. Please first use the path command.");
+                        StartScan();
+                        return;
                     }
 
                     if (potentialFullName == "filter")
                     {
-                        if (GetFollowingNumber(expectedArgument, args, i) == -1)
-                        {
-                            Console.WriteLine("Please enter a valid number to filter by in weeks.");
-                            return;
-                        }
-                        
-                        filterAction.FilterService(scanAction.files, GetFollowingNumber(expectedArgument, args, i));
+                        StartFilter(args, expectedArgument, i);
+                        return;
+                    }
+
+                    if (potentialFullName == "showList")
+                    {
+                        _showListAction.Action();
+                        return;
                     }
 
                     if (potentialFullName == "move")
                     {
-                        GetFollowingPath(expectedArgument, args, i);
-                        moveAction.MoveAction(expectedArgument.Value);
+                        StartMove(args, expectedArgument, i);
+                        return;
                     }
 
                     if (potentialFullName == "delete")
                     {
-                        deleteAction.DeleteAction();
+                        StartDelete();
+                        return;
                     }
                     
                 }
             }
         }
+        
+        Console.WriteLine("No valid command found.");
+        ShowHelp();
     }
-
+    private static void ShowHelp()
+    {
+        string helpText = File.ReadAllText(HelpFile);
+        Console.WriteLine(helpText);
+    }
+    
     private void GetFollowingPath(ExpectedArguments argument, string[] args, int foundIndex)
     {
         if (foundIndex + 1 >= args.Length)
         {
             Console.WriteLine("Please enter a valid path to a directory.");
+            File.WriteAllText("path.txt", " ");
             return;
         }
         
@@ -132,19 +159,56 @@ public class ArgumentParser
         if(CheckPath(followingValue))
         {
             argument.Value = followingValue;
-            File.WriteAllText(pathFile, argument.Value);
+            File.WriteAllText(PathFile, argument.Value);
+        }
+        else
+        {
+            File.WriteAllText("path.txt", " ");
         }
     }
-    
     public bool CheckPath(string args)
     {
-            if (Directory.Exists(args))
-            {
-                return true;
-            }
+        if (args == "C:\\" || args == "/" || args == "D:\\")
+        {
+            Console.WriteLine("I am a coward and do not operate directly on a root directory.");
             return false;
+        }
+            
+        if (Directory.Exists(args))
+        {
+            return true;
+        }
+        return false;
     }
+    private void StartScan()
+    {
+        if(!File.Exists(PathFile))
+        {
+            Console.WriteLine("Please use first the path command.");
+            ShowHelp();
+            return;
+        }
+        
+        string pathText = File.ReadAllText(PathFile);
 
+        if (string.IsNullOrWhiteSpace(pathText))
+        {
+            Console.WriteLine("No path to scan. Please first use the path command.");
+            return;
+        }
+
+        _scanAction.ScanFiles(pathText);
+    }
+    private void StartFilter(string[] args, ExpectedArguments expectedArgument, int i)
+    {
+        if (GetFollowingNumber(expectedArgument, args, i) == -1)
+        {
+            Console.WriteLine("Please enter a valid number to filter by in weeks.");
+            return;
+        }
+                        
+        _filterAction.FilterService(GetFollowingNumber(expectedArgument, args, i));
+    }
     private int GetFollowingNumber(ExpectedArguments argument, string[] args, int foundIndex)
     {
         string followingValue = args[foundIndex + 1];
@@ -156,4 +220,16 @@ public class ArgumentParser
 
         return number;
     }
+    private void StartMove(string[] args, ExpectedArguments expectedArgument, int i)
+    {
+        GetFollowingPath(expectedArgument, args, i);
+                        
+        string pathText = File.ReadAllText(PathFile);
+        _moveAction.MoveAction(pathText);
+    }
+    private void StartDelete()
+    {
+        _deleteAction.DeleteAction();
+    }
+
 }
