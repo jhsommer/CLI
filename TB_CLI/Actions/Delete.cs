982f4b7fd61dae834d092934a2c4066b25a5951a
jhsommer <129003263+jhsommer@users.noreply.github.com>
2025-11-30 15:36:21 +0100

    deleted static keyword

--- TB_CLI/Actions/Delete.cs
+++ TB_CLI/Actions/Delete.cs
@@ -4,15 +4,18 @@ public class Delete
 {
     private static List<string> Load(string content)
     {
+        if (!File.Exists(content))
+        {
+            return new List<string>();
+        }
+        
         return File.ReadAllLines("lists.txt").ToList();  
     }
     
-    private static string content = File.ReadAllText("lists.txt");
-    
-    private List<string> filteredFiles = Load(content);
-    
     public void DeleteAction()
     {
+        List<string> filteredFiles = Load("lists.txt");
+        
         foreach (string file in filteredFiles)
         {
             File.Delete(file);  
--- TB_CLI/Actions/Move.cs
+++ TB_CLI/Actions/Move.cs
@@ -2,18 +2,20 @@ namespace TB_CLI.Actions;
 
 public class Move
 {
-    private static List<string> Load(string content)
+    private List<string> Load(string content)
     {
+        if (!File.Exists(content))
+        {
+            return new List<string>();
+        }
+        
         return File.ReadAllLines("lists.txt").ToList();  
     }
     
-    
-    private static string content = File.ReadAllText("lists.txt");
-    
-    private List<string> filteredFiles = Load(content);
-    
     public void MoveAction(string newPath)
     {
+        List<string> filteredFiles = Load("lists.txt");
+        
         foreach (string file in filteredFiles)
         {
             File.Move(file, newPath);
